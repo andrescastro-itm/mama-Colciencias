0c86265a97e8dc02acb30065e10d840003f675da
Diana228 <diana.mar09@hotmail.com>
2019-08-13 10:39:41 -0500

    Update main.py

--- Mascaras/main.py
+++ Mascaras/main.py
@@ -1,24 +1,16 @@
-'''Este código toma los datos de los ROIS desde el csv (centro, numero de slices, etc) 
+'''Este código toma los datos de los ROIS desde el csv (centro, número de slices, etc) 
 y genera las máscaras '''
 
 import numpy as np
 import os
-#import Funcion_mascara
 from Funcion_mascara import mascara
 
 
-#csv=np.loadtxt('/home/dianamarin/6.ROIS MRI/Generar_Mascaras/data_roi/DATA_f_1.csv',
-#               delimiter=',',dtype='str',encoding='latin1')  # Cargar el csv con la información de los ROIS
+csv=np.loadtxt('/ruta_del_archivo/Archivo.csv',
+               delimiter=',',dtype='str',encoding='latin1') # Cargar el archivo Data_3_slices.csv 
 
 
-#csv=np.loadtxt('/home/dianamarin/6.ROIS MRI/Generar_Mascaras/DATA_FINAL/DATA_FINAL_PUNTOS.csv',
-#               delimiter=',',dtype='str',encoding='latin1')
-
-csv=np.loadtxt('/home/dianamarin/6.ROIS MRI/Generar_Mascaras/DATA_FINAL/Data_3_slices.csv',
-               delimiter=',',dtype='str',encoding='latin1')
-
-
-Pat1='/home/dianamarin/6.ROIS MRI/CDR_PACS_NRRD'   # Path a estudios guardados en nrrd 
+Pat1='/ruta/Estudios_NRRD'   # Path a estudios guardados en formato nrrd 
 Rutas=np.array([])
 
 for directorio,_,subdirectorio in os.walk(Pat1):
@@ -30,14 +22,13 @@ def encontrar_ruta_nrrd(name,secuencia):
     for i in range(0,len(Rutas)):
         if name +'/' in Rutas[i]:
             if secuencia in Rutas[i]:
-#                print('POSICION',i)
                 return Rutas[i]
 #                a=Rutas[i]
 #               
 #    return a
                
 
-for i in range(400,600):#(1,len(csv))   (515,516)
+for i in range(1,len(csv))
     ruta='WARNING'
     ruta=encontrar_ruta_nrrd(csv[i,0],csv[i,2])
     print(i)
