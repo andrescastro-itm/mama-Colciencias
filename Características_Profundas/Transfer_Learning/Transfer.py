28e562c7223bb696994584bf197353420f4ea5c6
Diana228 <diana.mar09@hotmail.com>
2019-09-07 17:46:53 -0500

    Update Transfer.py

--- Características_Profundas/Transfer_Learning/Transfer.py
+++ Características_Profundas/Transfer_Learning/Transfer.py
@@ -9,7 +9,6 @@ from keras.layers import Input
 from keras.models import Model
 from keras.utils import np_utils
 from sklearn.utils import shuffle
-#from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import train_test_split
 from keras.applications.imagenet_utils import _obtain_input_shape
 from imagenet_utils import decode_predictions
@@ -19,24 +18,10 @@ from keras.callbacks import ModelCheckpoint
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 x = np.load('/home/dianamarin/6.ROIS MRI/DEEP_AND_RADIOMICS_FEATURES/Arreglos_Red/Datos_01/FAS2_ROIS146_224x224x3.npy')
-#y = np.load('/home/dianamarin/6.ROIS MRI/DEEP_AND_RADIOMICS_FEATURES/Arreglos_Red/Datos_01/l_ADC_01_Clases.npy')
 
 model = ResNet50(weights='imagenet',include_top=False)
 
-'''  Nombres de las capas a tomar
-res2a_branch2b 
-res2c_branch2c 
-res3d_branch2c 
-res4f_branch2c 
-res5c_branch2c 
-
-'''
-
 layer_name = 'res2a_branch2b'
 intermediate_layer_model = Model(inputs=model.input,outputs=model.get_layer(layer_name).output)
-Features_block1_conv2 = intermediate_layer_model.predict(x)
-
-
-
-
+Features_res2a_branch2b = intermediate_layer_model.predict(x)
 
